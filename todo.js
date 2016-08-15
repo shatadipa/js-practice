6a97012b7b065e229fdd2bbb6b2950d93c2af3a0
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2016-08-15 01:38:27 -0700

    Incorporates CR Changes

--- todo.js
+++ todo.js
@@ -1,34 +1,36 @@
-var ToDo = function(title, description){
-    this.title = title;
-    this.description = description;
-    this.isDone = false;
+var ToDo = function(title, description) {
+  this.title = title;
+  this.description = description;
+  this.isDone = false;
 };
 
-var ToDoList = [];
-
-Array.prototype.addToDo = function(title, description){
-    var todo = new ToDo(title, description);
-    this.push(todo);
+var ToDoList = function() {
+  this.items = [];
 };
 
-Array.prototype.removeToDo = function(title){
-  for(var i=0;i<this.length;i++){
-     if(this[i].title===title){
-        this.splice(i,1);
-        console.log(title + ' deleted successfully!');
-        return;
-     }
+ToDoList.prototype.addToDo = function(title, description) {
+  var todo = new ToDo(title, description);
+  this.items.push(todo);
+};
+
+ToDoList.prototype.removeToDo = function(title) {
+  for (var i = 0; i < this.items.length; i++) {
+    if (this.items[i].title === title) {
+      this.items.splice(i, 1);
+      console.log(title + ' deleted successfully!');
+      return;
+    }
   }
   console.log(title + ' not found!')
 };
 
-Array.prototype.markDone = function(title){
-  for(var i=0;i<this.length;i++){
-     if(this[i].title===title){
-        this[i].isDone = true;
-        console.log(title + ' marked done!');
-        return;
-     }
+ToDoList.prototype.markDone = function(title) {
+  for (var i = 0; i < this.items.length; i++) {
+    if (this.items[i].title === title) {
+      this.items[i].isDone = true;
+      console.log(title + ' marked done!');
+      return;
+    }
   }
   console.log(title + ' not found!')
-}
+};
